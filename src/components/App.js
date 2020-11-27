8c7a0952d46ca4ff6ae54373a79c3bd0971bbe26
Matthew Hill <matthill500@hotmail.com>
2020-11-27 14:17:00 +0000

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
+import React,{Component} from 'react';
 import '../css/App.css';
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <div>Add Appointments</div>
-              <div>Search Appointments</div>
-              <div>List Appointments</div>
+class App extends Component {
+  render (){
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <div>Add Appointments</div>
+                <div>Search Appointments</div>
+                <div>List Appointments</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './css/index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+
 import 'jquery/dist/jquery.js';
 import 'popper.js/dist/umd/popper.js';
 import 'bootstrap/dist/js/bootstrap.js';
+
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,8 +16,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
